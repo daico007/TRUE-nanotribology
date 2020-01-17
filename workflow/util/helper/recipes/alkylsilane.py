9f0ba4f8c47091725d32ccf7fa02471d0d75d773
Co Quach <daico007@gmail.com>
2020-01-17 13:57:40 -0600

    fix the mb.example issue

--- workflow/util/helper/recipes/alkylsilane.py
+++ workflow/util/helper/recipes/alkylsilane.py
@@ -1,5 +1,5 @@
 import mbuild as mb
-from mbuild.examples.alkane.alkane import Alkane
+from .alkane import Alkane
 from mbuild.lib.moieties import Silane
 from util.helper.recipes.one_port import Methyl
 class Alkylsilane(mb.Compound):
